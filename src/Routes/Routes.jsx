6579c6f31e5b2b20ecb0234f98e2c5e08ccc2369
mostak9 <mostak.ahm263@gmail.com>
2023-10-17 00:26:58 +0600

    setup user management system client side and set router setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1 className="text-4xl font-black">This is client side</h1>
+    }
+])
+
+export default router;
