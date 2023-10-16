1d6fdac8f0fa64e6ebbaa6ddcc91f6d1e6e8dc58
mostak9 <mostak.ahm263@gmail.com>
2023-10-17 02:10:33 +0600

    pass user info by POST mehtod through server api to database

--- src/Components/CreateUser/CreateUser.jsx
+++ src/Components/CreateUser/CreateUser.jsx
@@ -2,6 +2,29 @@ import { Link } from "react-router-dom";
 import { IoIosArrowRoundBack } from "react-icons/io";
 
 const CreateUser = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const gender = form.gender.value;
+        const status = form.status.value;
+
+        const user = {name, email, gender, status};
+        console.log(user);
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
   return (
     <div>
       <Link to={"/"} className="px-5 py-3 shadow-lg font-medium ">
@@ -14,7 +37,7 @@ const CreateUser = () => {
           <p className="text-gray-400">Use below form to create new account</p>
         </div>
         <div className="mt-6">
-          <form action="" className=" space-y-6">
+          <form onSubmit={handleSubmit} className=" space-y-6">
             {/* name input */}
             <div className="flex flex-col gap-2">
               <label htmlFor="name" className="text-gray-400 font-medium">
@@ -26,6 +49,7 @@ const CreateUser = () => {
                 name="name"
                 placeholder="Type name here"
                 className="input input-bordered w-full"
+                required
               />
             </div>
             {/* email input */}
@@ -39,6 +63,7 @@ const CreateUser = () => {
                 name="email"
                 placeholder="Type email here"
                 className="input input-bordered w-full"
+                required
               />
             </div>
             {/* Gender field */}
@@ -46,11 +71,11 @@ const CreateUser = () => {
                 <p className="font-medium text-gray-400">Gender</p>
                 <div className="flex gap-12 items-center">
                     <div className="flex items-center gap-3">
-                        <input type="radio" id="male" name="gender" className="radio checked:bg-sky-600"/>
+                        <input type="radio" id="male" value='male' name="gender" className="radio checked:bg-sky-600" required/>
                         <label htmlFor="male" className="font-medium">Male</label>
                     </div>
                     <div className="flex items-center gap-3">
-                        <input type="radio" id="female" name="gender" className="radio checked:bg-sky-600"/>
+                        <input type="radio" id="female" value='female' name="gender" className="radio checked:bg-sky-600" required/>
                         <label htmlFor="female" className=" font-medium">Female</label>
                     </div>
                 </div>
@@ -60,11 +85,11 @@ const CreateUser = () => {
                 <p className="font-medium text-gray-400">Status</p>
                 <div className="flex gap-12 items-center">
                     <div className="flex items-center gap-3">
-                        <input type="radio"id="active" name="status" className="radio checked:bg-sky-600"/>
+                        <input type="radio"id="active" value="active" name="status" className="radio checked:bg-sky-600" required/>
                         <label htmlFor="active" className=" font-medium">Active</label>
                     </div>
                     <div className="flex items-center gap-3">
-                        <input type="radio" id="inactive" name="status" className="radio checked:bg-sky-600"/>
+                        <input type="radio" id="inactive"  value='inactive' name="status" className="radio checked:bg-sky-600" required/>
                         <label htmlFor="inactive" className="font-medium">Inactive</label>
                     </div>
                 </div>
