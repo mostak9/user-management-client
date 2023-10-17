67352efd65307065bb9dac96213190fcdce71573
mostak9 <mostak.ahm263@gmail.com>
2023-10-17 14:49:01 +0600

    added firesbase configuration

--- src/Components/AllUsers/AllUsers.jsx
+++ src/Components/AllUsers/AllUsers.jsx
@@ -1,9 +1,42 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { BiSolidUser } from "react-icons/bi";
-import {MdOutlineModeEditOutline, MdOutlineDelete} from 'react-icons/md'
+import { MdOutlineModeEditOutline, MdOutlineDelete } from "react-icons/md";
+import swal from "sweetalert";
+import { useState } from "react";
 
 const AllUsers = () => {
-    const loadedUsers =  useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+  const handleDelete = (_id) => {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this the user!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        fetch(`http://localhost:5000/users/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+
+                const remaining = users.filter(user => user._id !== _id)
+                setUsers(remaining);
+
+              swal("User deleted successfully", {
+                icon: "success",
+              });
+            }
+          });
+      } else {
+        swal("User not deleted!");
+      }
+    });
+  };
   return (
     <div>
       <Link to={"/createUser"} className="px-5 py-3 shadow-lg font-medium ">
@@ -24,20 +57,27 @@ const AllUsers = () => {
             </tr>
           </thead>
           <tbody>
-
-            {
-                loadedUsers.map((user, idx) => <tr key={user._id}>
-                    <th>{idx+1}</th>
-                    <td>{user?.name}</td>
-                    <td>{user?.email}</td>
-                    <td>{user?.gender}</td>
-                    <td>{user?.status}</td>
-                    <td className="flex items-center gap-3">
-                        <button title="Edit user" className="btn btn-sm shadow-md"><MdOutlineModeEditOutline className="text-xl"/></button>
-                        <button title="Delete user" className="btn btn-sm shadow-md"><MdOutlineDelete className="text-xl"/></button>
-                    </td>
-                  </tr>)
-            }
+            {users?.map((user, idx) => (
+              <tr key={user._id}>
+                <th>{idx + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>{user.status}</td>
+                <td className="flex items-center gap-3">
+                  <button title="Edit user" className="btn btn-sm shadow-md">
+                    <MdOutlineModeEditOutline className="text-xl" />
+                  </button>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    title="Delete user"
+                    className="btn btn-sm shadow-md"
+                  >
+                    <MdOutlineDelete className="text-xl" />
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.API_KEY ,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET ,
+  messagingSenderId: import.meta.env.MASSAGING_SENDER_ID ,
+  appId: import.meta.env.APP_ID ,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
